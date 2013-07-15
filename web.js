9f3af2688e00b93fc4dc0b54d7391e4b53bcbd4e
shenan1984 <shenan1984@gmail.com>
2013-07-15 02:50:09 +0000

    attempt 7 at task 7

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     var out = fs.readFileSync('index.html');
-    response.send(out);
+    response.send(out.toString());
 });
 
 var port = process.env.PORT || 5000;
