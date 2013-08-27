f46da80ef3f88313d83391c7c1f94b9c6f1c2322
shenan1984 <shenan1984@gmail.com>
2013-08-27 18:37:15 +0000

    remove old unused files

--- node_setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# setup node and some npm stuff
-sudo add-apt-repository ppa:chris-lea/node.js
-sudo apt-get update
-sudo apt-get install -y nodejs npm
-
-# install express
-npm install express
